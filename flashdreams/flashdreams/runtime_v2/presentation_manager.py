faea2fbc075260f5585880ba63fde5e7b5381aa6
Ariel Glasroth <aglasroth@nvidia.com>
2026-08-28 05:00:22 -0700

    swap default drop oldest due to quality risks

--- flashdreams/flashdreams/runtime_v2/presentation_manager.py
+++ flashdreams/flashdreams/runtime_v2/presentation_manager.py
@@ -49,7 +49,7 @@ class PresentationManager:
             ValueError: ``device`` is neither a CPU nor CUDA device.
         """
         self._buffer: queue.Queue[tuple[int, list[StepResult]]] = queue.Queue(maxsize=2)
-        self._backpressure_mode = BackpressureMode.DROP_OLDEST
+        self._backpressure_mode = BackpressureMode.BLOCK
         self._stop = threading.Event()
         self._put_timeout = 1.0 / 30.0
         self._counter_lock = threading.Lock()
